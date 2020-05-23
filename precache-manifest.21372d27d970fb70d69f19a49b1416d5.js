c89df68426bc3169ba80a4d6e74d53a4bc425631
Rakibul Islam Prince <rakibulislamprince07@gmail.com>
2020-05-23 13:25:16 +0600

    Updates

---
+++ precache-manifest.21372d27d970fb70d69f19a49b1416d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79ca99dcf5705932bbc744bccbfd379",
+    "url": "/emob/index.html"
+  },
+  {
+    "revision": "5aa6a69f7a4cec601017",
+    "url": "/emob/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a1110bde7eb0d326a5ed",
+    "url": "/emob/static/css/main.d89fb763.chunk.css"
+  },
+  {
+    "revision": "5aa6a69f7a4cec601017",
+    "url": "/emob/static/js/2.5112a3fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emob/static/js/2.5112a3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1110bde7eb0d326a5ed",
+    "url": "/emob/static/js/main.876a4a8f.chunk.js"
+  },
+  {
+    "revision": "d44e785a3763f1626bd1",
+    "url": "/emob/static/js/runtime-main.14ece905.js"
+  },
+  {
+    "revision": "fbc5d504786b2e176f2390f9118e4eb2",
+    "url": "/emob/static/media/stripped_bg5.fbc5d504.svg"
+  }
+]);
